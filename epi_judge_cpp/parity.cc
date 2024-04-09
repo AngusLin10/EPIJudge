f3ff020e02280c1a03a2b3374a3511583cb408d0
Angus Lin <Angus.lin@microtek.com.tw>
2024-04-09 09:00:44 +0800

    Fix 5.1 Parity

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,30 +1,35 @@
 #include "test_framework/generic_test.h"
+
+// reference: https://www.geeksforgeeks.org/finding-the-parity-of-a-number-efficiently/
+
+// Method 1
 short Parity(unsigned long long x) {
-    int result = 0;
+    short result = 0;
     while (x > 0) {
-        result ^= 1; // result = !result  
-        x &= (x - 1);
+        result ^= 1;  // Since result is initially 0 or 1, result ^= 1; is equivalent to result = !result;
+        x &= (x - 1); // Drops the lowest set bit of x. example: x = 1000 0101, x & (x - 1) = 1000 0100
     }
     return result;
+}
 
-    // https://www.geeksforgeeks.org/finding-the-parity-of-a-number-efficiently/
-    /*
+// Method 2
+short Parity2(unsigned long long x) {
     x ^= x >> 32;
     x ^= x >> 16;
     x ^= x >> 8;
     x ^= x >> 4;
     x ^= x >> 2;
     x ^= x >> 1;
-    */
+
+    return x & 1;
 
     /*
-    n = 1000 0101
-    n = n ^ (n >> 4) = 1000 ^ 0101 = 1101
-    n = n ^ (n >> 2) = 11 ^ 01 = 10
-    n = n ^ (n >> 1) = 1 ^ 0 = 1
-    Final result = n & 1 = 1
-    
-    return x & 1;
+    example:
+        n = 1000 0101
+        n = n ^ (n >> 4) = 1000 ^ 0101 = 1101
+        n = n ^ (n >> 2) = 11 ^ 01 = 10
+        n = n ^ (n >> 1) = 1 ^ 0 = 1
+        Final result = n & 1 = 1
     */
 }
 
