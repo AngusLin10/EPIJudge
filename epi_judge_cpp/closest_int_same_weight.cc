81cfd0a8a82efde5ade19dbea8983f28f9e32e54
AngusLin10 <loveangela17@gmail.com>
2024-04-13 21:54:50 +0800

    5.4 Closest int sam weight

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,31 @@
 #include "test_framework/generic_test.h"
+// Method1 O(n)
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    const int kNumUnsignBits = 64;
+
+    for (int i = 0; i < kNumUnsignBits - 1; ++i) {
+        if (((x >> i) & 1) != ((x >> (i + 1)) & 1)) {
+            x ^= (1ULL << i) | (1ULL << (i + 1));
+            return x;
+        }
+    }
+}
+
+// Method2 O(1)
+unsigned long long ClosestIntSameBitCount2(unsigned long long x) {
+    unsigned long long lowestBitNotSet = ~x & (x + 1);
+    unsigned long long lowestBitSet = x & ~(x - 1);
+
+    if (lowestBitNotSet > lowestBitSet) {
+        x |= lowestBitNotSet;
+        x ^= lowestBitNotSet >> 1;
+    }
+    else {
+        x ^= lowestBitSet;
+        x |= lowestBitSet >> 1;
+    }
+
+    return x;
 }
 
 int main(int argc, char* argv[]) {
