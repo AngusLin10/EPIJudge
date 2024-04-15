ce0c11b55c59b2605aae263fc13a75037ca5e8a6
Angus Lin <Angus.lin@microtek.com.tw>
2024-04-15 16:49:17 +0800

    Fix 5.5 Multiply without arithmetical operators

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,7 @@
 #include "test_framework/generic_test.h"
 unsigned long long Add(unsigned long long x, unsigned long long y) {
-    unsigned long long carry = 0;
     unsigned long long sum = 0;
+    unsigned long long carry = 0;
     while (x) {
         sum = x ^ y;
         carry = (x & y) << 1;
@@ -27,7 +27,6 @@ unsigned long long Multiply(unsigned long long x, unsigned long long y) {
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
-  Multiply(13, 9);
   return GenericTestMain(args, "primitive_multiply.cc",
                          "primitive_multiply.tsv", &Multiply,
                          DefaultComparator{}, param_names);
