4afe5f783d4b27417559d8976651eb54f7047921
Angus Lin <Angus.lin@microtek.com.tw>
2024-04-22 17:10:46 +0800

    5.9 Palindrome Number

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,7 +1,18 @@
 #include "test_framework/generic_test.h"
+// O(n)
 bool IsPalindromeNumber(int x) {
-  // TODO - you fill in here.
-  return true;
+  // x < 0 or last digit of the number is 0, x is not a palindrome.
+  if (x < 0 || (x > 0 && x % 10 == 0))
+    return false;
+
+  // Reverse half of the number
+  int reversed = 0;
+  while (x > reversed) {
+    reversed = reversed * 10 + x % 10;
+    x /= 10;
+  }
+  // odd number of digits, remove the middle digit
+  return x == reversed || x == reversed / 10;
 }
 
 int main(int argc, char* argv[]) {
