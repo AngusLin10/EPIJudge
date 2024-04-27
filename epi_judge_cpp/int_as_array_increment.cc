2bdefc4ecac75cf868a63e8b8fc8657e8f994336
AngusLin10 <loveangela17@gmail.com>
2024-04-27 22:50:44 +0800

    6.2 plus one

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -2,9 +2,17 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+// O(n)
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+    int index = A.size();
+    while (--index >= 0) {
+        ++A[index];
+        A[index] %= 10;     // If the current digit becomes 10, set it to 0 and carry over the 1 to the next digit.
+        if (A[index] != 0)  // If the current digit is not 0 after incrementing, no need to carry over, so return the result.
+            return A;
+    }
+    A.insert(begin(A), 1);
+    return A;
 }
 
 int main(int argc, char* argv[]) {
