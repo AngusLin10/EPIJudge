21d61076a68f1676066ee65b86d3c4a80c21b3a5
Angus Lin <Angus.lin@microtek.com.tw>
2024-04-29 13:42:14 +0800

    6.3 Multiply Integer

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -3,8 +3,25 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
-  // TODO - you fill in here.
-  return {};
+  if (num1.front() == 0 || num2.front() == 0) {
+    return {0};
+  }
+  const int sign = (num1.front() < 0) ^ (num2.front() < 0) ? -1 : 1;
+  num1.front() = abs(num1.front());
+  num2.front() = abs(num2.front());
+  vector<int> result(num1.size() + num2.size(), 0);
+  for (int i = num1.size() - 1; i >= 0; --i) {
+    for (int j = num2.size() - 1; j >= 0; --j) {
+      result[i + j + 1] += num1[i] * num2[j];
+      result[i + j] += result[i + j + 1] / 10;
+      result[i + j + 1] %= 10;
+    }
+  }
+  while (result.front() == 0) {
+    result.erase(result.begin());
+  }
+  result.front() *= sign;
+  return result;
 }
 
 int main(int argc, char* argv[]) {
