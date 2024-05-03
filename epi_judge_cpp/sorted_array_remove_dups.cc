1e2d680b645f84f15da82e1d0b7d63e634b42c27
AngusLin10 <loveangela17@gmail.com>
2024-05-03 19:16:30 +0800

    6.4 Delete duplicates from a sorted array

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -4,9 +4,20 @@
 #include "test_framework/timed_executor.h"
 using std::vector;
 // Returns the number of valid entries after deletion.
+// O(n)
 int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
+    vector<int>& A = *A_ptr;
+    if (A.size() == 0)
+        return 0;
+    int slow = 0, fast = 0;
+    while (fast < A.size()) {
+        if (A[slow] != A[fast]) {
+            A[++slow] = A[fast];
+        }
+        ++fast;
+    }
+
+    return slow + 1;
 }
 vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
   int end = executor.Run([&] { return DeleteDuplicates(&A); });
@@ -17,6 +28,8 @@ vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A"};
+  vector<int> test{ 1, 2, 2, 2, 3, 3, 4};
+  DeleteDuplicates(&test);
   return GenericTestMain(
       args, "sorted_array_remove_dups.cc", "sorted_array_remove_dups.tsv",
       &DeleteDuplicatesWrapper, DefaultComparator{}, param_names);
