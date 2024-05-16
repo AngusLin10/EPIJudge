9115c5a2e4600679af93acb4165b4c916eb5b299
Angus Lin <Angus.lin@microtek.com.tw>
2024-05-16 15:18:43 +0800

    6.17 Spiral Ordering of a 2D array

--- epi_judge_cpp/spiral_ordering.cc
+++ epi_judge_cpp/spiral_ordering.cc
@@ -3,8 +3,42 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> MatrixInSpiralOrder(const vector<vector<int>>& square_matrix) {
-  // TODO - you fill in here.
-  return {};
+    vector<int> result;
+    if (square_matrix.size() == 0 || square_matrix[0].size() == 0) return result;
+    int top = 0, bottom = square_matrix.size() - 1;
+    int left = 0, right = square_matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse from left to right along the top row
+        for (int i = left; i <= right; ++i) {
+            result.push_back(square_matrix[top][i]);
+        }
+        ++top;
+
+        // Traverse from top to bottom along the right column
+        for (int i = top; i <= bottom; ++i) {
+            result.push_back(square_matrix[i][right]);
+        }
+        --right;
+
+        if (top <= bottom) {
+            // Traverse from right to left along the bottom row
+            for (int i = right; i >= left; --i) {
+                result.push_back(square_matrix[bottom][i]);
+            }
+            --bottom;
+        }
+
+        if (left <= right) {
+            // Traverse from bottom to top along the left column
+            for (int i = bottom; i >= top; --i) {
+                result.push_back(square_matrix[i][left]);
+            }
+            ++left;
+        }
+    }
+
+    return result;
 }
 
 int main(int argc, char* argv[]) {
