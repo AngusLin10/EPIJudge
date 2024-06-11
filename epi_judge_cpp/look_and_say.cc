caf32f916bcf36b30589a202aff05a6d7c1b284a
Angus Lin <Angus.lin@microtek.com.tw>
2024-06-11 15:45:41 +0800

    7.9 Roman to Decimal

--- epi_judge_cpp/look_and_say.cc
+++ epi_judge_cpp/look_and_say.cc
@@ -4,13 +4,25 @@
 using std::string;
 
 string LookAndSay(int n) {
-  // TODO - you fill in here.
-  return "";
+  string result = "1";
+  while (--n) {
+    string next;
+    for (int i = 0; i < result.size(); ++i) {
+      int count = 1;
+      while (result[i] == result[i + 1] && i < result.size() - 1) {
+        ++count, ++i;
+      }
+      next += std::to_string(count) + result[i];
+    }
+    result = std::move(next);
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
+  LookAndSay(10);
   return GenericTestMain(args, "look_and_say.cc", "look_and_say.tsv",
                          &LookAndSay, DefaultComparator{}, param_names);
 }
