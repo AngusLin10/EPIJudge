e1cef9dfa81784a0772c8f85223489a339581a5b
Angus Lin <Angus.lin@microtek.com.tw>
2024-06-12 13:26:16 +0800

    7.10 All Valid IP addresses

--- epi_judge_cpp/valid_ip_addresses.cc
+++ epi_judge_cpp/valid_ip_addresses.cc
@@ -6,9 +6,25 @@
 using std::string;
 using std::vector;
 
+bool isValid(const string s) {
+  return !(s.size() > 3 || (s.size() > 1 && s[0] == '0') || stoi(s) > 255);
+}
+
 vector<string> GetValidIpAddress(const string& s) {
-  // TODO - you fill in here.
-  return {};
+  vector<string> result;
+  for (int i = 1; i < 4 && i < s.size(); ++i) {
+    for (int j = i + 1; j < i + 4 && j < s.size(); ++j) {
+      for (int k = j + 1; k < j + 4 && k < s.size(); ++k) {
+        string first = s.substr(0, i);
+        string second = s.substr(i, j - i);
+        string third = s.substr(j, k - j);
+        string fourth = s.substr(k, s.size() - k);
+        if (isValid(first) && isValid(second) && isValid(third) && isValid(fourth))
+          result.emplace_back(first + '.' + second + '.' + third + '.' + fourth);
+      }
+    }
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
