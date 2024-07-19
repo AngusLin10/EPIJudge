83aa82b0e822be3b9af7b8f2daef825dca6ec6e1
Angus Lin <Angus.lin@microtek.com.tw>
2024-07-19 13:59:57 +0800

    8.2 Reverse a single sublist

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -1,10 +1,26 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
-shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
-                                         int finish) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start, int finish)
+{
+  if (!L) return nullptr;
+  shared_ptr<ListNode<int>> dummy = make_shared<ListNode<int>>(0, L);
+  shared_ptr<ListNode<int>> p0 = dummy;
+  for (int i = 0; i < start - 1; ++i)
+    p0 = p0->next;
+
+  shared_ptr<ListNode<int>> prev = nullptr, curr = p0->next;
+  for (int i = 0; i < finish - start + 1; ++i) {
+    shared_ptr<ListNode<int>> next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  p0->next->next = curr;
+  p0->next = prev;
+
+  return dummy->next;
 }
 
 int main(int argc, char* argv[]) {
