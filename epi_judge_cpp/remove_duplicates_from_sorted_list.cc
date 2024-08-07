4211e5d4e62b10b23cfe9c6a28ff508a443e07a7
Angus Lin <Angus.lin@microtek.com.tw>
2024-08-07 17:02:47 +0800

    8.8 Remove Duplicates from a Sorted List

--- epi_judge_cpp/remove_duplicates_from_sorted_list.cc
+++ epi_judge_cpp/remove_duplicates_from_sorted_list.cc
@@ -4,12 +4,24 @@
 #include "test_framework/generic_test.h"
 using std::shared_ptr;
 
-shared_ptr<ListNode<int>> RemoveDuplicates(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> RemoveDuplicates(const shared_ptr<ListNode<int>> &L)
+{
+  shared_ptr<ListNode<int>> dummy_head = make_shared<ListNode<int>>(0, L);
+  shared_ptr<ListNode<int>> node = dummy_head->next;
+
+  while (node) {
+    if (node->next && node->data == node->next->data) {
+      node->next = node->next->next;
+    } else {
+      node = node->next;
+    }
+  }
+
+  return dummy_head->next;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L"};
   return GenericTestMain(args, "remove_duplicates_from_sorted_list.cc",
