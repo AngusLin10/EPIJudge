b6c9aeb4f45dbbe1cc22a994de974108af20418a
Angus Lin <Angus.lin@microtek.com.tw>
2024-08-16 16:39:49 +0800

    8.11 Linked List Palindromic

--- epi_judge_cpp/is_list_palindromic.cc
+++ epi_judge_cpp/is_list_palindromic.cc
@@ -1,12 +1,36 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
-bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
+bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L)
+{
+  shared_ptr<ListNode<int>> slow = L;
+  shared_ptr<ListNode<int>> fast = L;
+  while (fast && fast->next) {
+    fast = fast->next->next;
+    slow = slow->next;
+  }
+  shared_ptr<ListNode<int>> first_half_head = L;
+  shared_ptr<ListNode<int>> second_half_head = nullptr;
+  while (slow) {
+    auto temp = slow->next;
+    slow->next = second_half_head;
+    second_half_head = slow;
+    slow = temp;
+  }
+
+  while (first_half_head && second_half_head) {
+    if (first_half_head->data != second_half_head->data) {
+      return false;
+    }
+    first_half_head = first_half_head->next;
+    second_half_head = second_half_head->next;
+  }
+
   return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L"};
   return GenericTestMain(args, "is_list_palindromic.cc",
