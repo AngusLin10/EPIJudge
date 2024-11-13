c3fc35e7cf4884d81d8e180264456d4558c66a03
Angus Lin <Angus.lin@microtek.com.tw>
2024-11-13 17:09:39 +0800

    9.2 Evaluate RPN expression

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -2,9 +2,46 @@
 
 #include "test_framework/generic_test.h"
 using std::string;
+
+// O(n)
 int Evaluate(const string& expression) {
-  // TODO - you fill in here.
-  return 0;
+    std::stack<int> stack;
+    string number;
+    for (auto& c : expression) {
+        if (std::isdigit(c)) {
+            number += c;
+        }
+        else if (c == ',' && !number.empty()) {
+            stack.emplace(std::stoi(number));
+            number = "";
+        }
+        else if (c != ',' && stack.size() > 1) {
+            int y = stack.top();
+            stack.pop();
+            int x = stack.top();
+            stack.pop();
+            switch (c) {
+                case '+':
+                    stack.emplace(x + y);
+                    break;
+                case '-':
+                    stack.emplace(x - y);
+                    break;
+                case '*':
+                    stack.emplace(x * y);
+                    break;
+                case '/':
+                    stack.emplace(x / y);
+                    break;
+            }
+        }
+    }
+
+    if (!number.empty()) {
+        stack.emplace(std::stoi(number));
+    }
+
+    return stack.top();
 }
 
 int main(int argc, char* argv[]) {
