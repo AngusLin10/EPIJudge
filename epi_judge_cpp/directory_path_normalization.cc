e70d01dd6c4fcc9756f0b8b4f8ee2c73c77c1096
Angus Lin <Angus.lin@microtek.com.tw>
2024-11-19 10:42:31 +0800

    9.4 Normalize pathnames

--- epi_judge_cpp/directory_path_normalization.cc
+++ epi_judge_cpp/directory_path_normalization.cc
@@ -3,8 +3,27 @@
 #include "test_framework/generic_test.h"
 using std::string;
 string ShortestEquivalentPath(const string& path) {
-  // TODO - you fill in here.
-  return "";
+    std::stack<string> dirs;
+    std::stringstream stream(path);
+    string dir;
+    while (std::getline(stream, dir, '/')) {
+        if (!dir.empty() && dir != ".") {
+
+            dirs.emplace(dir);
+        }
+    }
+
+    string answer;
+
+    while (!dirs.empty()) {
+        answer = dirs.top() + "/" + answer;
+        dirs.pop();
+    }
+
+    if (!answer.empty() && answer.back() == '/')
+        answer.pop_back();
+
+    return answer;
 }
 
 int main(int argc, char* argv[]) {
